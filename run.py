4c63d22d957e1837645132319c9e79491adcb71f
jvcm <jvcm@cin.ufpe.br>
2020-02-04 23:43:22 -0300

    almost finished

--- run.py
+++ run.py
@@ -1,31 +1,25 @@
-from flask import Flask, jsonify, make_response, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, Markup
 import os
 from utils.functions import allow_image, process_img
-from werkzeug.utils import secure_filename
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-import time
 import keras
 import io
-import random
-from flask import Response
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
-
-p = [0,0]
+import base64
 
 app = Flask(__name__)
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG"]
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+# @app.route('/about')
+# def about():
+#     return render_template("about.html")
 
 @app.route('/diagnosis', methods = ['POST', 'GET'])
 def upload_image():
@@ -45,24 +39,21 @@ def upload_image():
                 classes = ['Normal', 'Pneumonia']
                 pred_class = classes[int(round(prediction))]
 
-                global p 
-                p = [100 - prob, prob]
+                c = [100 - prob, prob]
 
-                return render_template('result.html', classification = pred_class, probability = prob)
+                imagem = get_encoded_image(c)
+
+                return render_template('result.html',
+                    classification = pred_class, 
+                    probability = prob, 
+                    img_code= Markup('<img src=data:image/png;base64,{} style="height:250px; width:auto;" alt="Responsive image">'.format(imagem)))
             else:
                 feedback = "File format not allowed."
                 return render_template("diagnosis.html", feedback=feedback)
                     
     return render_template('diagnosis.html')
 
-@app.route('/plot.png')
-def plot_png():
-    fig = create_figure()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
-def create_figure():
+def get_encoded_image(c):
 
     fontsize= 12
     plt.rcParams['font.size'] = fontsize
@@ -70,14 +61,16 @@ def create_figure():
 
     fig = plt.Figure(figsize = (3.5,3.5))
     axis = fig.add_subplot(1, 1, 1)
-    axis.pie(p, autopct='%.1f%%', colors= cols)
+    axis.pie(c, autopct='%.1f%%', colors= cols)
     fig.legend(labels=['Normal', 'Pneumonia'])
 
     circle = plt.Circle(xy=(0, 0), radius=0.7, facecolor='white')
     fig.gca().add_artist(circle)
-
-    return fig
-
+    img = io.BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return plot_url
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
