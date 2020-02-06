4d0fa273ea31041f49370ee4f29a4a672ed87b2b
jvcm <jvcm@cin.ufpe.br>
2020-02-06 20:46:40 -0300

    optmize tensor flow

--- flask/run.py
+++ flask/run.py
@@ -1,4 +1,109 @@
-from app import app
+from flask import Flask, request, render_template, Markup
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import base64
+import tensorflow as tf
+from tensorflow.python.keras.backend import set_session
+from tensorflow.python.keras.models import load_model
+
+app = Flask(__name__, template_folder='./app/templates/')
+
+def load():
+    global sess
+    sess = tf.Session()
+    global graph
+    graph = tf.get_default_graph()
+    set_session(sess)
+    global new_model
+    new_model = load_model('./app/model.h5')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/diagnosis', methods = ['POST', 'GET'])
+def upload_image():
+    if request.method == 'POST':
+        if request.files:
+            image = request.files['imgInp']
+
+            if allow_image(image.filename, ["JPEG", "JPG", "PNG"]):
+
+                test_img = process_img(image, 150, 32)
+                with graph.as_default():
+                    set_session(sess)
+                    prediction = new_model.predict(test_img)[0][0]
+
+                prob = 100*round(prediction, 3)
+                classes = ['Normal', 'Pneumonia']
+                pred_class = classes[int(round(prediction))]
+
+                c = [100 - prob, prob]
+
+                imagem = get_encoded_image(c)
+
+                return render_template('result.html',
+                    classification = pred_class, 
+                    probability = prob, 
+                    img_code= Markup('<img src=data:image/png;base64,{} style="height:250px; width:auto;" alt="Responsive image">'.format(imagem)))
+            else:
+                feedback = "File format not allowed."
+                return render_template("diagnosis.html", feedback=feedback)
+                    
+    return render_template('diagnosis.html')
+
+def allow_image(filename, imageExtensions):
+    if filename == "":
+        print("No filename")
+        return False
+    elif not "." in filename:
+        print("No . in filename")
+        return False
+    elif not filename.split('.')[-1].upper() in imageExtensions:
+        print("Invalid format")
+        return False
+    else:
+        return True
+
+def process_img(img_path, img_dims, batch_size):
+    test_data = []
+
+    img = plt.imread(img_path)
+    if len(img.shape) > 2:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (img_dims, img_dims))
+    img = np.dstack([img, img, img])
+    img = img.astype('float32') / 255
+    test_data.append(img)
+        
+    test_data = np.array(test_data)
+    
+    return test_data
+
+def get_encoded_image(c):
+
+    fontsize= 12
+    plt.rcParams['font.size'] = fontsize
+    cols = ['#66b3ff', '#ff9999']
+
+    fig = plt.Figure(figsize = (3.5,3.5))
+    axis = fig.add_subplot(1, 1, 1)
+    axis.pie(c, autopct='%.1f%%', colors= cols)
+    fig.legend(labels=['Normal', 'Pneumonia'])
+
+    circle = plt.Circle(xy=(0, 0), radius=0.7, facecolor='white')
+    fig.gca().add_artist(circle)
+    img = io.BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return plot_url
 
 if __name__ == '__main__':
+    load()
     app.run()
